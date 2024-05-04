297026155b3e88eaa1d3db91d945fc6df6d0c5e6
Flugel <aidenlangba@gmail.com>
2024-05-04 17:44:36 +0530

    codeium

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,4 +37,17 @@ return {
   		},
   	},
   },
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    lazy = false,
+    config = function()
+        require("codeium").setup({
+          enable_chat = true
+        })
+    end
+},
 }
